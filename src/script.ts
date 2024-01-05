8ea59e90478181f859f973e9fbbcd01867a63c81
andres fernandez <andresfernandez.mdp@gmail.com>
2024-01-05 07:39:58 -0300

    sumOptimized function

--- src/script.ts
+++ src/script.ts
@@ -12,3 +12,20 @@ export function sum(nums: number[], requiredSum: number): boolean {
   }
   return false;
 }
+
+export function sumOptimized(nums: number[], requiredSum: number): boolean {
+  if (nums.length === 0) {
+    throw new Error("Empty array");
+  } else if (nums.length === 1) {
+    throw new Error("Array must have at least two elements");
+  }
+  const numbersTest = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = requiredSum - nums[i];
+    if (numbersTest.has(complement)) {
+      return true;
+    }
+    numbersTest.add(nums[i]);
+  }
+  return false;
+}
