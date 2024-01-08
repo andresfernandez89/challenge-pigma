838098744a7466b0e840b1e4fa3db931cde66f59
andres fernandez <andresfernandez.mdp@gmail.com>
2024-01-08 20:28:36 -0300

    comments

--- src/test/performance/measureExecutionTIme.ts
+++ src/test/performance/measureExecutionTIme.ts
@@ -1,15 +1,20 @@
-import { sum,sumOptimized } from "../../script.js";
+import { sum, sumOptimized } from "../../script.js";
 
 const nums = [1, 2, 4, 4];
 const requiredSum = 8;
-
-function measureExecutionTime(fn: (nums:number[], requiredSum:number)=>void, name:string) {
+// Mide el tiempo de ejecución de una función utilizando el método `performance.now().
+// Recibe una función como primer argumento y un nombre como segundo argumento.
+// Retorna el tiempo de ejecucion en milisisegundos de la funcion.
+function measureExecutionTime(
+  fn: (nums: number[], requiredSum: number) => void,
+  name: string,
+) {
   const startTime = performance.now();
-  const result = fn(nums, requiredSum);
+  fn(nums, requiredSum);
   const endTime = performance.now();
   console.log(`${name} execution time: ${endTime - startTime} milliseconds`);
-  return result;
+  return;
 }
 
-measureExecutionTime(sum, 'sum function');
-measureExecutionTime(sumOptimized, 'sumOptimized function');
+measureExecutionTime(sum, "sum function");
+measureExecutionTime(sumOptimized, "sumOptimized function");
